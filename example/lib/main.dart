fb97de2261b157010481ece0e6fc5f5f7c829847
Henry.Liang <henryliang3027@gmail.com>
2025-06-20 17:16:42 +0800

    refactor: :recycle: add sample data

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:example/example.dart';
+import 'package:example/my_home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
       debugShowCheckedModeBanner: false,
     );
